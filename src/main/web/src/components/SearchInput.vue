ffe79623aaf615647064b186966e52cb62a5117c
Jachin <1106025235@qq.com>
2025-06-17 03:58:50 +0800

    feat: 搜索支持

---
+++ src/main/web/src/components/SearchInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="relative">
+    <input
+      type="text"
+      v-model="searchQuery"
+      @input="handleSearch"
+      placeholder="搜索..."
+      class="w-full px-4 py-2 text-gray-700 bg-white border rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+    />
+    <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+      <svg
+        class="w-5 h-5 text-gray-400"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        ></path>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const searchQuery = ref('')
+
+const emit = defineEmits(['search'])
+
+const handleSearch = () => {
+  emit('search', searchQuery.value)
+}
+</script> 
--- src/main/web/src/views/AppSet.vue
+++ src/main/web/src/views/AppSet.vue
@@ -1,3 +1,7 @@
+<script setup>
+import {Menu, MenuButton, MenuItems, MenuItem} from "@headlessui/vue";
+import SearchInput from "../components/SearchInput.vue";
+</script>
 <script>
 import {addAppSet, deleteAppSet, getAppSetList, updateAppSet} from "../api/appSetApi.js";
 import {randomString} from "../utils/commUtil.js";
@@ -13,6 +17,7 @@ export default {
     async add() {
       if ((await addAppSet({
         name: "test",
+        description: "safa",
       })).success) {
         await this.getAppSetList();
       }
@@ -34,6 +39,17 @@ export default {
       const result = await getAppSetList();
       this.appSet = result.data;
       console.log(this.appSet);
+    },
+    async searchList(args) {
+      let params;
+      if (args && args !== "") {
+        params = {
+          name: args
+        }
+      }
+      const result = await getAppSetList(params);
+      this.appSet = result.data;
+      console.log(this.appSet);
     }
   },
   created() {
@@ -49,9 +65,14 @@ export default {
       <h1 class="text-3xl font-bold">App Set</h1>
     </div>
 
+    <div>
+      <SearchInput @search="searchList"/>
+    </div>
+
     <div class="flex my-4">
-      <div class="px-4 py-2 bg-gray-200 rounded-lg flex justify-between items-center hover:bg-gray-300 cursor-pointer select-none"
-           @click="add()">
+      <div
+          class="px-4 py-2 bg-gray-200 rounded-lg flex justify-between items-center hover:bg-gray-300 cursor-pointer select-none"
+          @click="add()">
         添加
       </div>
 
@@ -65,19 +86,47 @@ export default {
       <div class="flex flex-wrap">
         <div v-for="app in appSet" class="w-full md:w-1/4 xl:w-1/6 mr-4 my-2">
           <div class="bg-white rounded-lg shadow-lg overflow-hidden">
-            <div class="px-6 py-4">
+            <div class="px-6 py-4 relative">
               <div class="font-bold text-xl mb-2">{{ app.name }}</div>
               <p class="text-gray-700 text-base">
                 {{ app.description }}
               </p>
 
-              <div class="px-4 py-2 bg-gray-200 rounded-lg flex justify-between items-center hover:bg-gray-300 cursor-pointer"
-                   @click="update(app)">
-                修改
-              </div>
-              <div class="px-4 py-2 bg-gray-200 rounded-lg flex justify-between items-center hover:bg-gray-300 cursor-pointer"
-                   @click="del(app)">
-                删除
+              <div class="absolute top-1/4 right-2 z-50">
+                <Menu as="div" class="relative inline-block text-left">
+                  <div>
+                    <MenuButton
+                        class="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+                      More
+                    </MenuButton>
+                  </div>
+                  <transition
+                      enter-active-class="transition duration-100 ease-out"
+                      enter-from-class="transform scale-95 opacity-0"
+                      enter-to-class="transform scale-100 opacity-100"
+                      leave-active-class="transition duration-75 ease-in"
+                      leave-from-class="transform scale-100 opacity-100"
+                      leave-to-class="transform scale-95 opacity-0"
+                  >
+                    <MenuItems>
+                      <MenuItem>
+                        <div
+                            class="px-4 py-2 bg-gray-200 rounded-lg flex justify-between items-center hover:bg-gray-300 cursor-pointer"
+                            @click="update(app)">
+                          修改
+                        </div>
+
+                      </MenuItem>
+                      <MenuItem>
+                        <div
+                            class="px-4 py-2 bg-gray-200 rounded-lg flex justify-between items-center hover:bg-gray-300 cursor-pointer"
+                            @click="del(app)">
+                          删除
+                        </div>
+                      </MenuItem>
+                    </MenuItems>
+                  </transition>
+                </Menu>
               </div>
             </div>
           </div>
