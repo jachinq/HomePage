7b45ce9c691bbc39b806f4b1ad632e16e91992b8
jachin <jachinq@163.com>
2025-06-19 01:26:10 +0800

    feat: 全局引入toast

--- src/main/web/src/index.ts
+++ src/main/web/src/index.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './styles/index.css';
 import router from './router'
+import toast from './components/toast'
 
 const app = createApp(App);
 app.use(router)
+app.use(toast)
 app.mount('#root');
