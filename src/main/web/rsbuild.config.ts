f4b49e6b7cb5361925ee887c06318988887f4c27
Jachin <JachinQ@163.com>
2025-06-22 01:45:32 +0800

    feat: page setting

--- src/main/web/rsbuild.config.ts
+++ src/main/web/rsbuild.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginVue } from '@rsbuild/plugin-vue';
+import { describe } from 'node:test';
 
 export default defineConfig({
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   plugins: [pluginVue()],
+  html: {
+    title: 'Home Page',
+    favicon: './src/assets/favicon.ico',
+    meta: {
+      description: 'My Personal Home Page and App Entry',
+    }
+  },
   dev: {
     writeToDisk: false,
   },
