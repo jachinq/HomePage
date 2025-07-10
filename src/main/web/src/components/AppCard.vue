8a434306e2127fc585b5b7ee265fe76c821c26d4
Jachin <JachinQ@163.com>
2025-07-10 22:14:40 +0800

    feat: 优化代码

--- src/main/web/src/components/AppCard.vue
+++ src/main/web/src/components/AppCard.vue
@@ -1,35 +1,3 @@
-<template>
-
-  <div class="w-full sm:w-1/3 md:w-1/4 lg:w-1/6 xl:w-1/7 2xl:w-1/9 min-w-[200px]">
-    <div class="group bg-gray-800 rounded-lg shadow-lg overflow-hidden hover:bg-slate-700 cursor-pointer"
-         @click="openApp(app)">
-      <div class="px-6 py-4 relative">
-        <div class="flex items-center mb-2 gap-2">
-          <template v-if="props.app.icon">
-            <div v-if="props.app.icon.startsWith('http://') || props.app.icon.startsWith('https://') || props.app.icon.startsWith('data:image/')">
-                <img :src="props.app.icon" class="icon w-12 h-12 overflow-hidden middle-0 md:block" alt="">
-            </div>
-            <div v-else v-html="props.app.icon" class="icon w-12 h-12 overflow-hidden middle-0 md:block"></div>
-          </template>
-          <template v-else>
-            <div class="icon w-12 h-12 overflow-hidden middle-0 md:block">
-              <img :src="appIcon" alt="">
-            </div>
-          </template>
-
-          <span class="font-bold text-xl group-hover:underline max-w-full overflow-hidden whitespace-nowrap">{{ props.app.name }}</span>
-        </div>
-        <span class="text-gray-400 text-sm max-w-full overflow-hidden whitespace-nowrap" :title="props.app.description">
-                    {{ props.app.description }}
-                </span>
-
-        <div class="absolute top-1 right-[-4px] z-50 mx-2">
-          <slot></slot>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
 
 <script setup lang="ts">
 import {useToast} from "@/components/toast";
@@ -70,9 +38,13 @@ const outerDomainProc = (outerUrl: string): string => {
 
 const openApp = (app: AppSet) => {
   // console.log(configData.value)
-  if (!openWithInnerMode(app) || !openWithOuterMode(app)) {
-    toast.error("未配置内网地址或端口，无法使用内网模式打开")
+  if (openWithInnerMode(app)){
+    return;
+  } 
+  if (openWithOuterMode(app)) {
+    return
   }
+  toast.error("未正确配置端口或内网域名或外网地址，无法正常打开")
   // console.log(app)
 }
 
@@ -87,13 +59,12 @@ const openWithInnerMode = (app: AppSet) => {
     }
   }
   // console.log("innerMode is false")
-  return true;
+  return false;
 }
 
 const openWithOuterMode = (app: AppSet) => {
   if (!app.outerUrl || !props.configData.outerDomain) {
-    // console.log("未配置外网地址")
-    toast.error("未配置外网地址")
+    // toast.error("未配置外网地址")
     return false;
   }
   // console.log(outerDomainProc(app.outerUrl))
@@ -101,6 +72,39 @@ const openWithOuterMode = (app: AppSet) => {
   return true;
 }
 </script>
+
+<template>
+
+  <div class="w-full sm:w-1/3 md:w-1/4 lg:w-1/6 xl:w-1/7 2xl:w-1/9 min-w-[200px]">
+    <div class="group bg-gray-800 rounded-lg shadow-lg overflow-hidden hover:bg-slate-700 cursor-pointer"
+         @click="openApp(app)">
+      <div class="px-6 py-4 relative">
+        <div class="flex items-center mb-2 gap-2">
+          <template v-if="props.app.icon">
+            <div v-if="props.app.icon.startsWith('http://') || props.app.icon.startsWith('https://') || props.app.icon.startsWith('data:image/')">
+                <img :src="props.app.icon" class="icon w-12 h-12 overflow-hidden middle-0 md:block" alt="">
+            </div>
+            <div v-else v-html="props.app.icon" class="icon w-12 h-12 overflow-hidden middle-0 md:block"></div>
+          </template>
+          <template v-else>
+            <div class="icon w-12 h-12 overflow-hidden middle-0 md:block">
+              <img :src="appIcon" alt="">
+            </div>
+          </template>
+
+          <span class="font-bold text-xl group-hover:underline max-w-full overflow-hidden whitespace-nowrap">{{ props.app.name }}</span>
+        </div>
+        <span class="text-gray-400 text-sm max-w-full overflow-hidden whitespace-nowrap" :title="props.app.description">
+                    {{ props.app.description }}
+                </span>
+
+        <div class="absolute top-1 right-[-4px] z-50 mx-2">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style>
 .icon svg {
